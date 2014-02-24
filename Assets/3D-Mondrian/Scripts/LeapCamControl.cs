b45cfc779defaf9cf60924aeb49750fbec7c249c
Jan Menzel <menzel.jm@googlemail.com>
2014-02-24 07:52:56 +0100

    Inital import

---
+++ Assets/3D-Mondrian/Scripts/LeapCamControl.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapCamControl : MonoBehaviour {
+
+
+    Controller m_leapController;
+
+    public GameObject DirectedTo;
+    public float MoveSpeed = 0.5f;
+    public float HandHeightStart = 2f;
+    public float SmoothLerpFactor = 0.2f;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_leapController = new Controller();
+        if (transform == null)
+        {
+            Debug.LogError("LeapFly must have a parent object to control");
+        }
+	}
+	
+
+    void FixedUpdate()
+    {
+
+        Frame frame = m_leapController.Frame();
+
+        if (frame.Hands.Count >= 2)
+        {
+            if (frame.Fingers.Count < 3)
+            {
+                return;
+            }
+
+            Hand leftHand = GetLeftMostHand(frame);
+            Hand rightHand = GetRightMostHand(frame);
+
+            // takes the average vector of the forward vector of the hands, used for the
+            // pitch of the plane.
+            Vector3 avgPalmForward = (frame.Hands[0].Direction.ToUnity() + frame.Hands[1].Direction.ToUnity()) * 0.5f;
+
+            Vector3 handDiff = leftHand.PalmPosition.ToUnityScaled() - rightHand.PalmPosition.ToUnityScaled();
+
+            Vector3 avgPos = (leftHand.PalmPosition.ToUnityScaled() + rightHand.PalmPosition.ToUnityScaled()) / 2;
+
+            Vector3 newRot = transform.localRotation.eulerAngles;
+            newRot.z = -handDiff.y * 20.0f;
+
+            // adding the rot.z as a way to use banking (rolling) to turn.
+            newRot.y += handDiff.z * 3.0f - newRot.z * 0.03f * transform.rigidbody.velocity.magnitude;
+            newRot.x = -(avgPalmForward.y - 0.1f) * 100.0f;
+            //float forceMult = 20.0f;
+
+            // if closed fist, then stop the plane and slowly go backwards.
+            
+
+
+            //Debug.Log("x:" + avgPos.x + " -- y:" + avgPos.y + " -- z:" + avgPos.z);
+
+            transform.RotateAround(DirectedTo.transform.position, new Vector3(0, 1, 0), newRot.z / 100 * MoveSpeed);   //MoveSpeed * Time.deltaTime);
+
+            float pos = avgPos.y - HandHeightStart;
+
+            Vector3 newPosition = new Vector3(transform.position.x, (transform.position.y + pos) / 2, transform.position.z);
+
+            transform.position = Vector3.Lerp(transform.position, newPosition, SmoothLerpFactor);
+
+            if (DirectedTo != null)
+            {
+                transform.LookAt(DirectedTo.transform.position);
+            }
+        }
+
+    }
+
+
+    Hand GetLeftMostHand(Frame f)
+    {
+        float smallestVal = float.MaxValue;
+        Hand h = null;
+        for (int i = 0; i < f.Hands.Count; ++i)
+        {
+            if (f.Hands[i].PalmPosition.ToUnity().x < smallestVal)
+            {
+                smallestVal = f.Hands[i].PalmPosition.ToUnity().x;
+                h = f.Hands[i];
+            }
+        }
+        return h;
+    }
+
+    Hand GetRightMostHand(Frame f)
+    {
+        float largestVal = -float.MaxValue;
+        Hand h = null;
+        for (int i = 0; i < f.Hands.Count; ++i)
+        {
+            if (f.Hands[i].PalmPosition.ToUnity().x > largestVal)
+            {
+                largestVal = f.Hands[i].PalmPosition.ToUnity().x;
+                h = f.Hands[i];
+            }
+        }
+        return h;
+    }
+}
---
+++ Assets/3D-Mondrian/Scripts/MondrianBehaviour.cs
@@ -0,0 +1,85 @@
+﻿using System.IO.IsolatedStorage;
+using UnityEngine;
+
+namespace Assets
+{
+    public class MondrianBehaviour : MonoBehaviour
+    {
+        public float ObjectDistance = 0.01f;
+        public GameObject MonCube;
+        public float SplitLerpFactor = 0.5f;
+
+        private Color[] _supportedColors;
+        private uint _colorPointer;
+        
+        public void Start ()
+        {
+            _supportedColors = new[] { Color.red, Color.yellow, Color.blue, Color.black, Color.white };
+            _colorPointer = 0;
+        }
+
+        public void Update()
+        {
+        }
+
+        public void OnMouseDown()
+        {
+            //Split Horizontal
+            if (Input.GetKey(KeyCode.Y))
+            {
+                SplitHorizontal();
+            }
+
+            //Split Vertical
+            if (Input.GetKey(KeyCode.X))
+            {
+                SplitVertical();
+            }
+
+            //Change Color
+            if (Input.GetKey(KeyCode.C))
+            {
+                ChangeColour();
+            }
+        }
+
+        private void ChangeColour()
+        {
+            if(_colorPointer >= _supportedColors.Length)
+            {
+                _colorPointer = 0;
+            }
+
+            renderer.material.color = _supportedColors[_colorPointer++];
+        }
+
+        private void SplitVertical()
+        {
+            var scale = transform.localScale;
+            var position = transform.position;
+
+            transform.localScale = new Vector3(scale.x / 2, scale.y, scale.z);
+            transform.position = new Vector3(position.x - (scale.x / 4), position.y, position.z);
+
+
+            
+            var newPosition = new Vector3(position.x + (scale.x / 4), position.y, position.z);
+            var o = Instantiate(MonCube, newPosition, transform.rotation) as GameObject;
+
+            if (o != null) o.renderer.material.color = _supportedColors[0];
+        }
+
+        private void SplitHorizontal()
+        {
+            var scale = transform.localScale;
+            var position = transform.position;
+
+            transform.localScale = new Vector3(scale.x, (scale.y / 2), scale.z);
+            transform.position = new Vector3(position.x, position.y + (scale.y / 4), position.z);
+
+            var o = Instantiate(MonCube, new Vector3(position.x, position.y - (scale.y / 4), position.z), transform.rotation) as GameObject;
+
+            if (o != null) o.renderer.material.color = _supportedColors[0];
+        }
+    }
+}
